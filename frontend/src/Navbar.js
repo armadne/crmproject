c6d399c97de93502a0291279427901b325c8fd3b
armadne <madane.aroul@hotmail.com>
2025-03-16 16:54:56 +0100

    Integration navbar et logo

--- frontend/src/Navbar.js
+++ frontend/src/Navbar.js
@@ -6,16 +6,45 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => {
 return (
-    <nav className="navbar">
-        <h1>NavBar</h1>
-        <div>
-            <Link to="/">Accueil</Link>
-            <Link to="/register">Register</Link>
-            <Link to="/login">Se connecter</Link>
-            <Link to="/reviews">Give A Review</Link>
-        </div>
-    </nav>
+
+    <nav className="navbar navbar-dark bg-dark" style={{marginBottom:"30px"}}>
+    <div className="container d-flex justify-content-between align-items-center" style={{marginLeft:"15px"}}>
+    <img src="./logo-site.png" alt="logo" style={{width:"85px", height: "85px"}} />
+      <ul className="navbar-nav d-flex flex-row gap-3" style={{marginRight:"900px"}}>
+     
+        <li className="nav-item">
+          <Link className="nav-link text-white" to="/">Accueil</Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link text-white" to="/reviews">Give A Review</Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link text-white" to="/login">Se connecter</Link>
+        </li>
+      </ul>
+    </div>
+  </nav>
+
+
+
+
+
 )
     }
 
+
+
+
+
+
 export default Navbar;
+
+
+
+
+
+
+
+
+
+
